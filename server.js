764ea8ca026f7b9748867bb184d7995fa7adf389
Hussdin <Hussdin.c@mail.kmutt.ac.th>
2021-05-18 18:55:49 +0700

    edit database code creator

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
-const hostname = 'localhost';
-const port = 3000;
-const bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-const multer = require('multer');
-const path = require('path');
+const { rejects } = require('assert/strict');
 const mysql = require('mysql'); 
+const { resolve } = require('node:path');
+const { promises } = require('node:stream');
 
 const con = mysql.createConnection({
     host: "localhost",
-   
+    user : "root",
+    password: "",
+    database :"register_for_petsociety"
+})
+
+con.connect(err => {
+    if(err) throw(err);
+    else{
+        console.log("Connected")
+    }
+})
+
+
+const queryDB = (sql) => {
+    return new Promise((resolve,rejects) =>{
+        con.query(sql,(err,result,fields) =>{
+            if(err) rejects(err);
+            else 
+                resolve(result)
+        })
+    } )
+}
+
+app.post('/data',async(req,res) => {
+    let sql = "CREATE TABLE IF NOT EXISTS userInfo (fname VAECHAR(50),lname VARCHAR(50),gender VARCHAR(50),bd DATE), pettype VARCHAR(50),email VARCHAR(50),username VAECHAR(50),password VAECHAR(50),confirmpassword VAECHAR(50))";
+    let result = await queryDB(sql);
+    sql = `INSERT INTO userinfo (fname,lname,email,username,password,confirmpassword) VALUES("${req.body.Fname}","${req.body.Lname}","${req.body.email}","${req.body.username}","${req.body.pass}","${req.body.confirmpass}")`;
+    result = await queryDB(sql);
+    console.log("Save");
+    res.end("New saveed");
+
 })
