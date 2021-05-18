a9efccc9ce583a89c0a38fe65c222033f87d5fff
Hussdin <Hussdin.c@mail.kmutt.ac.th>
2021-05-18 20:23:45 +0700

    edit

--- signUp.js
+++ signUp.js
@@ -1,7 +1,11 @@
+const { METHODS } = require("node:http");
+
 const selected = document.querySelector(".selected");
 const optionsPet = document.querySelector(".options-pet")
 
 const optionList = document.querySelectorAll(".option")
+
+let Gender = "", Pet = "";
 
 selected.addEventListener("click",() => {
     optionsPet.classList.toggle("active");
@@ -13,3 +17,18 @@ optionList.forEach(o => {
         optionsPet.classList.remove("active");
     })
 })
+
+// for send data to DB
+
+function selectgender(event){
+    gender = event;
+}
+
+function selectpet(event){
+    pet = event;
+
+}
+function onCommit(){
+    
+
+}
